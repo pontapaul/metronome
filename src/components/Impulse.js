c335ced5a3e0ff17ff546327749e1ce4022f4623
pontapaul <pontapaul@hotmail.it>
2021-04-01 00:30:57 +0200

    Set impulse animation duration based on bpm prop

--- src/components/Impulse.js
+++ src/components/Impulse.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 class Impulse extends Component {
     render() {
         return <div className="impulse">
-            <div className="sphere">{this.props.bpm}</div>
+            <div className="sphere" style={{'animationDuration': (60/this.props.bpm) + 's'}}>{this.props.bpm}</div>
         </div>;
     }
 }
